077ac5a9b15ce651e1ce4af7d471f03d21f47b41
Giovanni <giorg1993@hotmail.com>
2018-10-11 00:48:26 -0500

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -1,19 +1,30 @@
-// =========
+// ========= ========= ========= =========
 // PUERTO
-// =========
-
+// ========= ========= ========= =========
 process.env.PORT = process.env.PORT || 3000;
 
-// =========
+// ========= ========= ========= =========
 //  ENTORNO
-// =========
-
+// ========= ========= ========= =========
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// =========
-// BASE DE DATOS
-// =========
+// ========= ========= ========= =========
+//  VENCIMIENTO DEL TOKEN
+// ========= ========= ========= =========
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
+// ========= ========= ========= =========
+//  SEED de Autenticación
+// ========= ========= ========= =========
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// ========= ========= ========= =========
+// BASE DE DATOS
+// ========= ========= ========= =========
 let urlDB;
 
 if(process.env.NODE_ENV === 'dev'){
--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json()) 
-app.use(require('./routes/usuario'));
+
+// Configuración global de rutas
+app.use(require('./routes/index'));
+
 
   mongoose.connect(process.env.URLDB, { useNewUrlParser: true, autoIndex: false } , (err, res)=>{
       if(err) throw err;
